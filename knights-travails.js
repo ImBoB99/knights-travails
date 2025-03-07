abdc7f339c1ee15ab8c0c2a2f8f2cb987df1b4ad
Dominik Ubuntu <119432963+ImBoB99@users.noreply.github.com>
2025-03-07 10:19:36 +0100

    initial commit

---
+++ knights-travails.js
@@ -0,0 +1,10 @@
+function knightMoves(startingVertex, finalVertex) {
+  // TO DO
+  // [x, y]
+  // 8x8 grid
+  // knight movement can be: 2 steps forward 1 side step || 1 step fowards 2 side steps
+  let knightPosition = startingVertex;
+  let knightDirections = [[1,2], [2, 1]]
+}
+
+knightMoves([0, 0], [1, 2]);
